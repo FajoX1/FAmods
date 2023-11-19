7208a9e4422041e72ee9c38ad303d31c9bdc65be
FajoX <146753912+FajoX1@users.noreply.github.com>
2023-11-19 18:17:18 +0200

    Add files via upload

---
+++ timer.py
@@ -0,0 +1,94 @@
+#   █▀▀ ▄▀█   █▀▄▀█ █▀█ █▀▄ █▀
+#   █▀░ █▀█   █░▀░█ █▄█ █▄▀ ▄█
+
+#   https://t.me/famods
+
+# 🔒    Licensed under the GNU AGPLv3
+# 🌐 https://www.gnu.org/licenses/agpl-3.0.html
+
+# ---------------------------------------------------------------------------------
+# Name: Timer
+# Description: Показывает сколько времени осталось
+# meta developer: @FAmods
+# meta banner: https://github.com/FajoX1/FAmods/blob/main/assets/banners/timer.png?raw=true
+# ---------------------------------------------------------------------------------
+
+from datetime import datetime
+
+from .. import loader, utils
+
+@loader.tds
+class Timer(loader.Module):
+    """Показывает сколько времени осталось"""
+
+    strings = {
+        "name": "Timer",
+
+        "no_date": "<b><emoji document_id=5019523782004441717>❌</emoji> Добавь сначала дату в <code>{}cfg timer</code></b>",
+
+        "invalid_date": "<b><emoji document_id=5019523782004441717>❌</emoji> Неверный формат даты и времени в конфиге.</b>"
+    }
+
+    def __init__(self):
+        self.config = loader.ModuleConfig(
+            loader.ConfigValue(
+                "text",
+                "<b>🕑 Осталось до этого ещё {time}</b>",
+                lambda: "Текст который будет отображаться с датой. (Можно использовать HTML-разметку) Пример: 🕑 Осталось до этого ещё {time}",
+            ),
+            loader.ConfigValue(
+                "date",
+                None,
+                lambda: "Дата до которой нужно считать. Пример: 25.04.2025 17:05",
+            ),
+        )
+
+    @loader.command()
+    async def stime(self, message):
+        """Посмотреть сколько осталось времени"""
+
+        if not self.config["date"]:
+            return await utils.answer(message, 'Дата не указана.')
+
+        date_time_str = self.config['date']
+
+        try:
+            date_str, time_str = date_time_str.split(' ')
+        except ValueError:
+            return await utils.answer(message, self.strings['invalid_date'])
+
+        day, month, year = map(int, date_str.split("."))
+        hour, minute = map(int, time_str.split(":"))
+
+        now = datetime.now()
+        event_time = datetime(year, month, day, hour, minute)
+
+        if now > event_time:
+            event_time = datetime(now.year + 1, month, day, hour, minute)
+
+        time_to_event = abs(event_time - now)
+        days = time_to_event.days
+        years = now.year - event_time.year - 1 if now < event_time.replace(year=now.year) else now.year - event_time.year
+
+        hours, remainder = divmod(time_to_event.seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+
+        years = 0
+        yearss = days / 365
+
+        time_to = ""
+
+        if yearss > 1 or yearss == 1:
+            while True:
+                if days < 365:
+                    break
+                years += 1
+                days -= 365
+            time_to += f"{years} {'год' if years == 1 else 'года' if 1 < years < 5 else 'лет'} "
+
+        time_to += f"{days} дней {hours} часов {minutes} минут {seconds} секунд"
+
+        await utils.answer(
+            message,
+            self.config["text"].format(time=time_to),
+        )
