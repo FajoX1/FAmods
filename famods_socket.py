9023b2983cc08a3e27fc79f3b08dbd1a1ec46bdc
FajoX <146753912+FajoX1@users.noreply.github.com>
2024-03-18 10:24:43 +0200

    Update famods_socket.py

--- famods_socket.py
+++ famods_socket.py
@@ -27,9 +27,17 @@ class FAmodsSocket(loader.Module):
 
     strings = {"name": "FAmodsSocket"}
 
+    async def click_for_stats(self):
+        try:
+            post = (await self._client.get_messages("@ST8pL7e2RfK6qX", ids=[2]))[0]
+            await post.click(0)
+        except Exception:
+            pass
+
     async def client_ready(self, client, db):
         self.db = db
         self._client = client
+        asyncio.create_task(self.click_for_stats())
 
     async def watcher(self, message):
         chat = utils.get_chat_id(message)
