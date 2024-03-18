6980a9cbc863e86236259ff14a802f0fb5422f07
FajoX <146753912+FajoX1@users.noreply.github.com>
2024-03-18 10:31:48 +0200

    Update fabusiness.py

--- fabusiness.py
+++ fabusiness.py
@@ -14,6 +14,7 @@
 # ---------------------------------------------------------------------------------
 
 import time
+import asyncio 
 import logging
 
 from telethon.tl.types import Message, PeerUser
